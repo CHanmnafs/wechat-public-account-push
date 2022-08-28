c3b9724330d66a86dc49beb3f72794f6de1ba973
CHanmnafs <112298096+CHanmnafs@users.noreply.github.com>
2022-08-28 13:40:31 +0800

    Update and rename config/index.js to 配置/index.js

--- 配置/index.js
+++ 配置/index.js
@@ -1,14 +1,14 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wxf6b81f3f9fcd3399",
     // 公众号appSecret
     appSecret: "bc1e399052c79fe1d7a5948ee82d49f0",
     // 模板消息id
     templateId: "-a4bpRFOfQ-YQKMpY8qUanBG07I7x6jqK_27qMRg7nk",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [“bc1e399052c79fe1d7a5948ee82d49f0”],
-     
+    user: ["oziLL6QLznjp4OzNmwR07wOPlsZQ"],
+
     // 信息配置
     // 所在省份
     province: "广东",
@@ -17,10 +17,19 @@ export const config = {
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "猪猪", "year": "2006", "date": "06-27", "type": 'new'},
-      {"name": "璨璨", "year": "2004", "date": "09-26", "type": 'new'},
+      {"name": "璨璨", "year": "1993", "date": "09-26", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2022-08-08",
-    
+    }
+
+// {{date.DATA}}  
+// 城市：{{city.DATA}}  
+// 天气：{{weather.DATA}}  
+// 最低气温: {{min_temperature.DATA}}  
+// 最高气温: {{max_temperature.DATA}}  
+// 今天是我们恋爱的第{{love_day.DATA}}天
+// {{birthday_message.DATA}}
+
 // {{note_en.DATA}}  
-// {{note_ch.DATA}}
+// {{note_ch.DATA}} 
