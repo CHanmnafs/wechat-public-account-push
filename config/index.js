e2d2e6028d3a87c0dc9c1b858ac57c5dc7f18455
CHanmnafs <112298096+CHanmnafs@users.noreply.github.com>
2022-08-28 12:05:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,27 +3,24 @@ export const config = {
     // 公众号appId
     appId: "",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "bc1e399052c79fe1d7a5948ee82d49f0",
     // 模板消息id
-    templateId: "",
+    templateId: "tIW1pXz4FWH8sPUFVDIzVq8sXR3pbUPRjGNCQQYOiLY",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: [“Wpqccc”],
      
     // 信息配置
     // 所在省份
     province: "广东",
     // 所在城市
-    city: "惠州",
+    city: "中山",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+      {"name": "猪猪", "year": "2006", "date": "06-27", "type": 'new'},
+      {"name": "璨璨", "year": "2004", "date": "09-26", "type": 'new'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
-    }
+    loveDate: "2022-08-08",
 
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
@@ -36,3 +33,9 @@ export const config = {
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
+    
+    # 金句中文，如果为空，默认会读取金山的每日金句
+"note_ch": "",
+# 金句英文
+"note_en": ""
+}
